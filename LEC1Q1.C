4fc76ae7c8e66c3fef6971c9d2b86c8c866c004f
Khushi-Khunt <khuntkhushi506@gmail.com>
2023-11-06 18:19:51 +0530

    assignment 1.1

---
+++ LEC1Q1.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   clrscr();
+   printf("Myself Khushi Khunt\n");
+   printf("I'm 21 years old\n");
+   printf("I have studied from satuababa vidhyalay\n");
+   printf("recently I am studying full stack development\n");
+   printf("Grid:5862\n");
+   printf("I m from surat");
+
+  getch();
+
+}
